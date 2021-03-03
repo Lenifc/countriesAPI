4cb5facd8dc4680433384436a36ff77d09cb7c75
Lenifc <p.baglaj@wp.pl>
2021-03-03 15:26:15 +0100

    Move loader to single component

---
+++ src/components/Loader.vue
@@ -0,0 +1,17 @@
+<template>
+  <img class="loader" src="@/assets/spinner.gif">
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.loader{
+   margin: 32px auto;
+   display: flex;
+   justify-content: center;
+}
+</style>
